c4e0fcdba9b8d42f7ae3091415854ecf02baab8e
Henok <henokmicrometre@gmail.com>
2023-08-16 22:40:35 +0100

    python modules

--- python/test.py
+++ python/test.py
@@ -2,8 +2,29 @@ import matplotlib
 import numpy
 import pandas
 import scipy
+
+"""
 print(numpy.__version__)
 print(numpy.__version__)
 print(pandas.__version__)
 print(matplotlib.__version__)
 print(scipy.__version__)
+
+"""
+
+
+
+def my_function():
+  print("Hello from a function")
+
+
+def my_function(fname):
+  print(fname + " Refsnes")
+
+
+
+def my_function(fname, lname):
+  print(fname, lname)
+
+
+my_function("Emil", "Refsnes")
