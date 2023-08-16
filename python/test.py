fdaf4206a1642151d0f4bb58f725291805f89267
Henok <henokmicrometre@gmail.com>
2023-08-16 22:16:33 +0100

    python modules

---
+++ python/test.py
@@ -0,0 +1,9 @@
+import matplotlib
+import numpy
+import pandas
+import scipy
+print(numpy.__version__)
+print(numpy.__version__)
+print(pandas.__version__)
+print(matplotlib.__version__)
+print(scipy.__version__)
